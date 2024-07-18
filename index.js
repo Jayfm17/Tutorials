45257dd3c0867c038e3a01851970c701dc1c8cfb
Jefferson Muthoka <Jayfm17@gmail.com>
2024-07-18 16:49:46 +0300

    Tutorials_1

---
+++ index.js
@@ -0,0 +1,73 @@
+x = 10;
+
+var y = 15;
+
+/* let course = "Web Development";
+
+//use const for variables that do not change
+
+const pi = 3.1452;
+
+//how to see the output let's display x
+console.log(pi);
+//assignment operator =
+
+//additional operator +
+console.log (x+y);
+
+//multiplication *
+
+//comparison operator /
+//comparison operator
+//greater than >
+//less than <
+// equal to ==
+//equal value and equal data type === return either true or false
+//greater than or equal to >=
+//less than or equal to <=
+
+console.log (++x);
+console.log(--x);
+console.log(x === y);
+
+
+//tenary operator ?
+//condition ? expression 1 : expression 2
+console.log(x < y ? 'yes': 'No');
+
+//datatypes
+
+//numbers: whole integer or decimals
+//strings: charcters 
+//boolean: true or false
+
+//array : collection of related items
+//to define array use this syntax
+let fruits =["apples","oranges","berries","watermelon"];
+console.log(fruits);
+
+//objects to store list of items it takes the form of pair to store details of a person
+let person ={name:"Jeff", course:"web", organization:"PLP"};
+console.log(person);
+
+//undefined
+let subject;
+
+//function 
+//A block of code that is designed to perform a particular task
+//To define a function functionName(){}
+//Can take several parameters and are applied within brackets
+function addition(a, b){
+    //instructions to be executed
+    return a+b;
+}
+console.log(addition(100, 200));
+
+//arrrow function () => { instructions to be executed}
+*/
+
+function multiply(x, y){
+    return x * y;
+}
+
+console.log(multiply(10, 15));
